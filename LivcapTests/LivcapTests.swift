2f90016fe84a5b210a52c88f249f14e622480ed5
wayne <lycazure@hotmail.com>
2025-06-02 13:58:19 -0500

    Initial Commit

---
+++ LivcapTests/LivcapTests.swift
@@ -0,0 +1,17 @@
+//
+//  LivcapTests.swift
+//  LivcapTests
+//
+//  Created by Rongwei Ji on 6/2/25.
+//
+
+import Testing
+@testable import Livcap
+
+struct LivcapTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
